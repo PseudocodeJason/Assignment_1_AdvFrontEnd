84ed9fbe93f801e39dc29a87cfa7a080d71d3253
ReaperMan2000 <jason.tieu@hotmail.com>
2022-03-04 18:39:38 -0500

    Added Comments for Logins

--- src/LoginForm/LoginForm.js
+++ src/LoginForm/LoginForm.js
@@ -2,10 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "react-bootstrap";
 
-// React uses virtual DOM to compare the new state of the DOM tree with the old state
-//DOM is Document Object Model
-//useNavigate is a hook that allows us to navigate to a new route
-//useNavigate === useHistory
+
 export const LoginForm = () => {
   console.log('Login form rendered');
   const user = {username: 'Admin', password: 'Admin'};
@@ -15,15 +12,14 @@ export const LoginForm = () => {
   <script>var Alert = ReactBootstrap.Alert;</script>
   const handleLogin = (event) => {
     if (username === user.username && password === user.password) {
+      //For admin control
       navigate("/Clothing/Admin");
     } else {
+      //only for vistors
       navigate("/Clothing/Visitor");
     }
   };
   
-  //useState is a hook (js function) that returns an array with two elements
-  // (data and a function to update the data)
-
   const form = (
     <div className="login-form">
       <h1>Login</h1>
@@ -47,6 +43,5 @@ export const LoginForm = () => {
       </Button>
     </div>
   );
-  //username = 'bobby' - WRONG
   return form;
 };
