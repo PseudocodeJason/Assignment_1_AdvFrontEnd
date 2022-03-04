0a151bd88d60fbe510d57ae1faab7a6a885c8636
ReaperMan2000 <jason.tieu@hotmail.com>
2022-03-04 17:29:20 -0500

    Change id

--- src/ClothingList/ClothingList.js
+++ src/ClothingList/ClothingList.js
@@ -18,12 +18,12 @@ export const ClothingList = () => {
   
 
   const [clothingList, setClothes] = useState([
-    {id: 0, name: "Hat", price: "$20.99", description: "You got to look nice", stock: 69},
-    {id: 1, name: "Shirt", price: "$35.98", description: "Pretty popular with the peoples", stock: 54},
-    {id: 2, name: "Pant", price: "$29.99", description: "I guess it be looking like pants", stock: 8},
-    {id: 3, name: "Sock", price: "$4.99", description: "Filled with holes idk why you would get it", stock: 564 },
-    {id: 4, name: "Shoes", price: "$49.99", description: "Selling Shoes but not in pairs", stock: 44},
-    {id: 5, name: "Ring", price: "$4.99", description: "Ahh well Its a ring", stock: 56},
+    {id: 1, name: "Hat", price: "$20.99", description: "You got to look nice", stock: 69},
+    {id: 2, name: "Shirt", price: "$35.98", description: "Pretty popular with the peoples", stock: 54},
+    {id: 3, name: "Pant", price: "$29.99", description: "I guess it be looking like pants", stock: 8},
+    {id: 4, name: "Sock", price: "$4.99", description: "Filled with holes idk why you would get it", stock: 564 },
+    {id: 5, name: "Shoes", price: "$49.99", description: "Selling Shoes but not in pairs", stock: 44},
+    {id: 6, name: "Ring", price: "$4.99", description: "Ahh well Its a ring", stock: 56},
   ])
 
   const filteredList = clothingList.filter(clothing => clothing.name.startsWith(filter));
@@ -42,7 +42,7 @@ export const ClothingList = () => {
 
   // This method isnt working
   const items = (name, price, des, stock) => {
-    const item = [{ id: clothingList.length , name: name , price: price, description: des, stock: stock}]
+    const item = [{ id: (clothingList.length + 1) , name: name , price: price, description: des, stock: stock}]
     handleAddItem (item)
   }
 
@@ -59,9 +59,9 @@ export const ClothingList = () => {
           </tr>
         </thead>
         <tbody>
-          {clothingList.map(({name, price, description, stock}, index) => (
+          {clothingList.map(({id, name, price, description, stock}, index) => (
             <tr key={index}>
-              <td>{index}</td>
+              <td>{id}</td>
               <td>{name}</td>
               <td>{price}</td>
               <td>{description}</td>
@@ -125,9 +125,9 @@ export const ClothingList = () => {
           </tr>
         </thead>
         <tbody>
-          {filteredList.map(({name, price, description, stock}, index) => (
+          {filteredList.map(({id, name, price, description, stock}, index) => (
             <tr key={index}>
-              <td>{index}</td>
+              <td>{id}</td>
               <td>{name}</td>
               <td>{price}</td>
               <td>{description}</td>
